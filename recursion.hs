5f49f0bdc402bbc4f576f9f94f0916e404fb391d
Aemylie <aemilie.bom@gmail.com>
2016-11-11 15:00:40 +0100

    6

--- recursion.hs
+++ recursion.hs
@@ -2,7 +2,7 @@
 --Hello recursion!
 -----------------------
 
---Home-made maximum if/else
+--Homemade maximum if/else
 maximumIf :: (Ord a)=>[a]->a
 maximumIf [] = error "maximum of empty list"
 maximumIf [x] = x
@@ -10,7 +10,7 @@ maximumIf (x:xs) = if x>maxTail then x else maxTail
 	where maxTail= maximum' xs
 
 
---Home-made maxumim guards
+--Homemade maxumim guards
 maximumG :: (Ord a)=>[a]->a
 maximumG [] = error "maximum of empty list"
 maximumG [x] = x
@@ -20,11 +20,23 @@ maximumG (x:xs)
 	where maxTail= maximum'' xs
 
 
---Home-made maximum max
-
-Er det her Haskell?
+--Homemade maximum max
 
 maximum' :: (Ord a)=> [a]->a
 maximum' [] = error "maximum of empty list"
 maximum' [x] = x
 maximum' (x:xs) = max x (maximum' xs)
+
+
+
+--SayMe modified (using guards and 'where')
+sayMe' :: Int -> String
+sayMe' x
+	|x<0 = "Say no to negativity"
+	|x<=(length sayNumbers) = sayNumbers !! x
+	|otherwise = "Woah, stop, that one is too big! I can only count so many"
+	where sayNumbers = ["zero!","one!","two!","three!","four!","five!"]
+
+-- This error occurs only with 6 as far as i know. Why?
+-- ghci>sayMe' 6
+-- "*** Exception: Prelude.!!: index too large 
