0381f03f156342b8a8eab394f8726e74e36575dd
Aemylie <aemilie.bom@gmail.com>
2016-11-15 15:41:45 +0100

    Perceptron v. 1

--- recursion.hs
+++ recursion.hs
@@ -1,42 +1,17 @@
------------------------
---Hello recursion!
------------------------
+xData :: [(Float,Float)]
+xData = [(0.6492, 10.5492), (5.0576, -1.9462), (-5.9590, 7.8897), (2.9614, 1.3547), (3.6815, 1.6019), (5.3024, 3.9243), (1.9835, 2.3669), (-3.4360, 8.0828), (6.1168, 2.3159), (6.2850, -0.4685), (4.4086, 1.3710), (-3.7105, 8.4309), (-2.3741, 6.1648), (0.4221, 8.5627), (-3.5980, 9.2361), (-4.5349, 9.6428), (1.6828, 0.5335), (5.3271, -1.5529), (3.2860, 3.1965), (5.2880, 1.2030), (-3.7126, 12.7188), (-2.5362, 6.8989), (-2.0253, 5.1877), (6.7019, 3.8357), (-2.9775, 8.5460), (2.4272, -0.4192), (3.7186, 4.0874), (-4.3252, 6.1897), (-4.8112, 9.7657), (-3.4481, 10.0994)]
+yData :: [Float]
+yData = [-1, 1, -1, 1, 1, 1, 1, -1, 1, 1, 1, -1, -1, -1, -1, -1, 1, 1, 1, 1, -1, -1, -1, 1, -1, 1, 1, -1, -1, -1]
 
---Homemade maximum if/else
-maximumIf :: (Ord a)=>[a]->a
-maximumIf [] = error "maximum of empty list"
-maximumIf [x] = x
-maximumIf (x:xs) = if x>maxTail then x else maxTail
-	where maxTail= maximum' xs
+infiniteData :: [a]->[a]
+infiniteData xs = xs ++ infiniteData xs
 
+scaleV :: Float -> (Float,Float)->(Float,Float)
+scaleV a (x1,x2) = (a*x1,a*x2)
 
---Homemade maxumim guards
-maximumG :: (Ord a)=>[a]->a
-maximumG [] = error "maximum of empty list"
-maximumG [x] = x
-maximumG (x:xs)
-	| x > maxTail = x
-	|otherwise = maxTail
-	where maxTail= maximumG xs
+addV :: (Float,Float)->(Float,Float)->(Float,Float)
+addV (x1,x2) (y1,y2) = (x1+y1,x2+y2)
 
-
---Homemade maximum max
-
-maximum' :: (Ord a)=> [a]->a
-maximum' [] = error "maximum of empty list"
-maximum' [x] = x
-maximum' (x:xs) = max x (maximum' xs)
-
-
-
---SayMe modified (using guards and 'where')
-sayMe' :: Int -> String
-sayMe' x
-	|x<0 = "Say no to negativity"
-	|x<=(length sayNumbers - 1) = sayNumbers !! x
-	|otherwise = "Woah, stop, that one is too big! I can only count so many"
-	where sayNumbers = ["zero!","one!","two!","three!","four!","five!"]
-
--- This error occurs only with 6 as far as i know. Why?
--- ghci>sayMe' 6
--- "*** Exception: Prelude.!!: index too large
+weight 0 _ _ = (1,1)
+weight k ys xs = addV (weight (k-1) ys xs) deltaw
+	where deltaw = scaleV ((infiniteData xs) !! (k-1)) ((infiniteData ys) !! (k-1)) 
