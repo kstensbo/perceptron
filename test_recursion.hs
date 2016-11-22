7cf6e8f1449bfb69c8ec35f2ca73db659dcd860a
Kristoffer Stensbo-Smidt <digitaldingo@linux.com>
2016-11-22 14:31:29 +0100

    Added function to update w until criterion is met.

--- test_recursion.hs
+++ test_recursion.hs
@@ -1,6 +1,30 @@
 x = [0..9]
+xs = cycle x
 
-syv (x:xs)
+syv2 :: [Integer] -> Integer
+syv2 (x:xs)
     | x == 7 = 7
-    | otherwise syv xs
+    | otherwise = syv2 xs
+
+syv :: [Integer] -> Integer
+syv (x:xs) = if x == 7
+               then 7
+               else syv xs
+
+
+kippers :: Integer -> Integer -> Integer
+kippers x w = w + x
+
+selleri :: [Integer] -> Integer -> Integer -> (Integer, Integer)
+selleri _ w 0 = (w, 0)
+selleri (x:xs) w n
+    | kippers x w > 100 = (w, n)
+    | otherwise = selleri xs (kippers x w) (n-1)
+
+
+-- sumLow' = sumLow 0
+--     where sumLow n xs
+--         |sum (take n xs)>100 = (sum (take n xs), n)
+--         |otherwise = sumLow (n+1) xs
+
 
